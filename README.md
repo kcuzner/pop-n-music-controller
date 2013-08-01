970e865f4e15edfd21280f8752923309e9a82875
Kevin Cuzner <kevin@kevincuzner.com>
2013-07-31 23:47:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Pop 'n Music PSX Controller
+### Kevin Cuzner
+
+
+AVR-based controller for the Pop 'n Music game supporting both native and
+emulator modes.
+
+## Goals
+
+* Recognized as a native controller by the PS1 version of the game
+* Recognized as a valid controller by the emulated version of the game
+
+## Notes
+
+### Cable
+
+* Yellow: ATT
+* Red: VCC
+* Black: GND
+* Blue: CLK
+* Orange: COMMAND
+* Green: ACK
+* Purple: DATA
+
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ * Pop 'n Music Controller
+ *
+ * This functions as a dual-mode PS1 controller for the game Pop 'n Music. The
+ * buttons are mapped as follows:
+ * - Emulator mode:
+ *   - 9 Buttons mapped in a way compatible with the emulators
+ *   - left, down, and right buttons not pressed
+ * - Playstation mode:
+ *   - 9 Buttons mapped the way that they are supposed to be
+ *   - left, down, and right buttons are pressed   
+ */
+
+int main(void)
+{
+    return 0;
+}
